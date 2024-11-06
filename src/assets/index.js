8104893466f481986663fe448a7f11bd166f62a1
Pavitar Rana <ranapavitar14@gmail.com>
2024-11-07 00:00:37 +0530

    done

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import close from "./close.svg";
+import logo from "./logo.svg";
+import menu from "./menu.svg";
+
+import algorithms from "./algorithms.png";
+import devnotes from "./devnotes.png";
+import oscs from "./oscs.png";
+import abarta from "./abarta.webp";
+import drlife from "./drlife.webp";
+
+export { algorithms, close, devnotes, logo, menu, oscs, abarta, drlife };
